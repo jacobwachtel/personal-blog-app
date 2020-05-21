ade39a0e7139f85d91f20146f267a74b13b639a9
Jacob Wachtel <jawachtel@gmail.com>
2020-05-21 12:45:41 -0500

    final: I think

--- exercises/js/new-post.js
+++ exercises/js/new-post.js
@@ -7,4 +7,25 @@ const API_URL = "http://localhost:3000/api/posts";
 const submitNewPost = () => {
     // HINT: Use FormData to store data to send over
     // HINT: Redirect the user to home page after successful submission
+    
+    const input = document.getElementById('post-image-file');
+    const title = document.getElementById('form-post-title').value;
+    const content = document.getElementById('form-post-content').value;
+
+    let formElem = new FormData();
+    formElem.append("post-image", input.files[0]);
+    formElem.append("title", title);
+    formElem.append("content", content);
+
+    fetch(API_URL, {
+        method: "POST",
+        body: formElem
+    }).then(()=>{
+        window.location.href = "index.html";
+        
+        // setTimeout(()=>{
+        //     window.location.href = "index.html";
+        // }, 1000);
+       
+    })
 }
